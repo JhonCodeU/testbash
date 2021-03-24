3a005c36c1fbde081d8f9ae89d52953298d092c7
Jhon deiber arcila castano <57459718+JhonCodeU@users.noreply.github.com>
2021-03-23 22:49:56 -0500

    agregando descripcion

--- ejercicio2.sh
+++ ejercicio2.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+#imprimir si un numero es de un solo digito o es de mas digitos 
 echo "Ejercicio 2, Ingrese un numero!"
 
 read number
+
+# -gt es igual a > aquí
 
 if [ $number -gt 10 ]
 then
